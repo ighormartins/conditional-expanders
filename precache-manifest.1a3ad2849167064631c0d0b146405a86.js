b616b7e2c96269788e619703a4792027712bff93
Ighor Martins <ighor.martins@gmail.com>
2021-03-22 13:06:17 +0000

    Updates

---
+++ precache-manifest.1a3ad2849167064631c0d0b146405a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9516af868d437158d9e25dd7db463b5a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fccd7110a4b43a4be18b",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "9ff27ac72c9d39d88de2",
+    "url": "./static/js/2.4dcddf95.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4dcddf95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fccd7110a4b43a4be18b",
+    "url": "./static/js/main.f083ad7b.chunk.js"
+  },
+  {
+    "revision": "c5aab25dd1c811a5560e",
+    "url": "./static/js/runtime-main.49aad576.js"
+  }
+]);
